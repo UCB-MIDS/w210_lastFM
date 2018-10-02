de003de30d9d3f067bc4d51666443b2645bb263e
Jayashree Raman <jayashreeraman@berkeley.edu>
2018-10-02 11:01:58 -0700

    Add files via upload

---
+++ postgres/load_user_profiles.sql
@@ -0,0 +1,11 @@
+﻿drop table user_profiles ;
+create table user_profiles (userid varchar(20),
+                            gender varchar(1),
+                            age	integer,
+                            country varchar(255),
+                            registered date) ;
+
+
+copy user_profiles from 'C:\Users\jayashree.raman\Documents\Learning\MIDS\capstone\lastfm-dataset-1K\userid-profile.tsv' with delimiter E'\t' null as '' format 'text'
+
+select * from user_profiles ;
