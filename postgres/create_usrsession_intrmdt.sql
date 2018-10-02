f9861b2db12c3c2dfb8fdee34464033522e1ab44
Jayashree Raman <jayashreeraman@berkeley.edu>
2018-10-02 11:02:34 -0700

    Add files via upload

---
+++ postgres/create_usrsession_intrmdt.sql
@@ -0,0 +1,12 @@
+﻿drop table usrsession_intrmdt ;
+create table usrsession_intrmdt as
+select userid, starttime, starttime::date startdate, 
+       case when date_part('hour', starttime) between 6 and 12 then '1'
+            when date_part('hour', starttime) between 12 and 17 then '2'
+            when date_part('hour', starttime) between 17 and 22 then '3'
+            else '4'
+       end timeofday,
+       lag(starttime,1) over (partition by userid order by userid, starttime) as  prevstart,
+       starttime-lag(starttime,1) over (partition by userid order by userid, starttime) as  delta
+from user_sessions 
+order by userid, starttime ;
