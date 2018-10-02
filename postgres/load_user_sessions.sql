6a5a20c31c33e01625bfc1c878e377165afb3440
Jayashree Raman <jayashreeraman@berkeley.edu>
2018-10-02 11:01:27 -0700

    Add files via upload

---
+++ postgres/load_user_sessions.sql
@@ -0,0 +1,7 @@
+﻿drop table user_sessions ;
+create table user_sessions (userid varchar(20),
+                            starttime timestamp ) ;
+
+
+copy user_sessions from 'C:\Users\jayashree.raman\Documents\Learning\MIDS\capstone\lastfm-dataset-1K\usrsession.tsv' with (format csv, delimiter E'\t', null '') ;
+
