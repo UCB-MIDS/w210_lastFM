914347d8a4cc99e689b55c2adf3c582f8a885ec5
Jayashree Raman <jayashreeraman@berkeley.edu>
2018-12-11 11:34:35 -0800

    Add files via upload

---
+++ Panel Data and Time Series models/ETL/create_user_sessions_aggregate.sql
@@ -0,0 +1,13 @@
+﻿drop table if exists user_session_aggregated_full ;
+create table user_session_aggregated_full (userid varchar(50), 
+                                      session_start timestamp, 
+                                      day_of_week integer,
+                                      time_of_day integer,
+                                      session_id integer, 
+                                      session_length integer, 
+                                      session_count integer,
+                                      absence_time integer,
+                                      gender integer, 
+                                      age integer, 
+                                      country integer,
+                                      genre text) ;
---
+++ Panel Data and Time Series models/ETL/create_usrsession_intrmdt.sql
@@ -0,0 +1,13 @@
+﻿drop table usrsession_intrmdt ;
+create table usrsession_intrmdt as
+select userid, session_start, session_dt, 
+       time_of_day,
+       day_of_week,
+       lag(session_start,1) over (partition by userid order by userid, session_start) as  prevstart,
+       session_start-lag(session_start,1) over (partition by userid order by userid, session_start) as  delta,
+       genre_1,
+       genre_2,
+       genre_3
+from user_session_genre 
+order by userid, session_start ;
+
