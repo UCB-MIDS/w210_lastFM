ea81129e21f76b51c24e37ef689289a2b65ca2ff
Jayashree Raman <jayashreeraman@berkeley.edu>
2018-12-11 14:56:26 -0800

    Add files via upload

---
+++ Panel Data and Time Series models/ETL/load_holidays.sql
@@ -0,0 +1,8 @@
+﻿drop table intnl_holidays ;
+create table intnl_holidays (id integer,
+                             raw_country text,
+                             hol_dt date,
+                             is_holiday integer,
+                             country text) ;
+
+copy intnl_holidays from 'C:\Users\jayashree.raman\Documents\Learning\MIDS\capstone\datasets\international_holidays.tsv' with (format csv, header true, delimiter E'\t', null '') ;
